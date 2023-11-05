3424eb069cf08a45baba1a8c129e703f81f56b41
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-11-05 09:54:44 -0500

    postpaid basic with styles

--- frontend/src/pages/PostpaidDashboard.jsx
+++ frontend/src/pages/PostpaidDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PrepaidCards from '../components/Postpaid Dashboard Components/PrepaidCards';
+import Header from '../components/Postpaid Dashboard Components/Header'
 
 
 export default function PostpaidDashboard() {
     return (
         <div className="flex flex-col w-full mt-8 ">
+            <Header />
             <div className="grid grid-cols-5 gap-1 w-full ">
                 {/* Left side for WalletCards */}
                 <div className="col-span-3">
