c2014141712f115d934436fc7b4734d5703d7583
Hasala <hasaladissanayake@yahoo.com>
2023-11-05 16:28:41 +0530

    added services

---
+++ Backend/payment/src/main/java/com/example/payment/PaymentApplication.java
@@ -0,0 +1,31 @@
+package com.example.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer CorsConfig(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedHeaders("*")
+						.allowedMethods("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+
+}
